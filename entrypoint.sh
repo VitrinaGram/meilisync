cf848b90184b93b1e74b83970965139515682bc4
Аркадий <arkptz@gmail.com>
2024-01-19 10:13:30 +0300

    update dockerfile

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ ls -R /meilisync
 
 # Заменяем плейсхолдеры переменными окружения и создаём конфигурационный файя
 
-envsubst < sync.example.yaml > sync.yaml
+envsubst < /meilisync/sync.example.yaml > /meilisync/sync.yaml
 
 # Запускаем основное приложение
 exec "$@"
