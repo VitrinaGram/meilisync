f2a7540e844ac434e4e9ca3cbd4a642ba09f7ed3
Аркадий <arkptz@gmail.com>
2024-01-19 09:19:19 +0300

    add ls to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-# Заменяем плейсхолдеры переменными окружения и создаём конфигурационный файл
+# Display the current directory structure
+ls -R /meilisync
+
+# Заменяем плейсхолдеры переменными окружения и создаём конфигурационный файя
+
 envsubst < sync.example.yaml > sync.yaml
 
 # Запускаем основное приложение
