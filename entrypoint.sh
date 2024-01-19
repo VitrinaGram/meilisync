e5a9bf2a2f4af178d642ae26141a70d483836eb1
Аркадий <arkptz@gmail.com>
2024-01-19 17:06:32 +0300

    upd

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ ls -l /meilisync
 
 # Заменяем плейсхолдеры переменными окружения и создаём конфигурационный файя
 
-envsubst < /meilisync/configs/sync.example.yaml > /meilisync/configs/sync.yaml
+envsubst < /meilisync/configs/sync.example.yaml > /meilisync/sync.yaml
 
 # Запускаем основное приложение
 exec "$@"
